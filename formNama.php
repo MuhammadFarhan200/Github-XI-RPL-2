119351e73be87bca6b644278d83aa0126e49ff2f
MuhammadFarhan200 <94825235+MuhammadFarhan200@users.noreply.github.com>
2022-01-29 15:20:34 +0700

    Add files via upload

---
+++ formNama.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Input</title>
+</head>
+<body>
+<h3>Nama Mahasiswa</h3>
+<form action="mahasiswa.php" method="post"><ol>
+<?php
+for ($a=1; $a <=1 ; $a++) {
+echo "<li> <input type='text' name='mahasiswa[$a]'>";}
+?>
+</ol><input type="submit" value=" Oke " /></form>
+</body>
+</html>
